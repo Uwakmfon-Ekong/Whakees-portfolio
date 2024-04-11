5463cd49b50f07a5486eeb7faff80f669e8a007c
Uwakmfon-Ekong <uwakmfone470@gmail.com>
2024-04-11 18:30:46 +0100

    added media queries

---
+++ script.js
@@ -0,0 +1,13 @@
+const checkbox = document.getElementById("checkbox");
+checkbox.addEventListener("change", () => {
+    document.body.classList.toggle("light");
+});
+
+
+const hamburger = document.getElementById('hamburger');
+const navItems= document.getElementById("navItems");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navItems.classList.toggle("active");
+})
